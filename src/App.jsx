61411079a0b1c23988bb93d8b1b680efa4ba3e3f
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-21 14:48:20 +0900

    ✨ show search results

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import client from "./client";
 import { ApolloProvider, Query } from "react-apollo";
 import { SEARCH_REPOSITORIES } from "./graphql";
@@ -37,11 +38,29 @@ const App = () => {
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
 
-          const repositoryCount = data.search.repositoryCount;
+          const search = data.search;
+          const repositoryCount = search.repositoryCount;
           const repositoryUnit =
             repositoryCount === 1 ? "Repository" : "Repositories";
           const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
-          return <h2>{title}</h2>;
+          return (
+            <>
+              <h2>{title}</h2>;
+              <ul>
+                {search.edges.map((edge) => {
+                  console.log(edge);
+                  const node = edge.node;
+                  return (
+                    <li key={node.id}>
+                      <a href={node.url}  target="_blank" >
+                        {node.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          );
         }}
       </Query>
     </ApolloProvider>
